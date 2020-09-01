63e2c7aab7823f1a0168258b5a6f5eb74bbb053f
rebazluqman <58704479+rebazluqman@users.noreply.github.com>
2020-09-01 18:47:04 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    TOKEN: 'NzIyOTIwMjcyMzc3Njc1ODgy.XuqF-A.k__MPSScox-bKBRWHiU4_-ZCw2M',
+    YT_API_KEY: 'AIzaSyDeoIH0u1e72AtfpwSKKOSy3IPp2UHzqi4',
+    prefix: '.',
+    ownerID: ['513049947105394708']
+}
